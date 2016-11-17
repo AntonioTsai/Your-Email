386616523044512ec63aa9e56339910ab846634e
Antonio Tsai <jimmyjack7777@gmail.com>
2016-11-18 00:57:49 +0800

    Remove Debug code

--- Bamboo Breakout/GameViewController.swift
+++ Bamboo Breakout/GameViewController.swift
@@ -34,8 +34,9 @@ class GameViewController: UIViewController {
         if let scene = GameScene(fileNamed:"GameScene") {
             // Configure the view.
             let skView = self.view as! SKView
-            skView.showsFPS = true
-            skView.showsNodeCount = true
+            // For Debug
+            //skView.showsFPS = true
+            //skView.showsNodeCount = true
             //skView.showsPhysics = true
             
             /* Sprite Kit applies additional optimizations to improve rendering performance */
