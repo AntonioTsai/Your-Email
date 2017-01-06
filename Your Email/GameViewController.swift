21ade945c6f58b0d49dd73e7a6a28b3b97417683
Antonio Tsai <jimmyjack7777@gmail.com>
2017-01-06 10:29:28 +0800

    Start game from menu

--- Your Email/GameViewController.swift
+++ Your Email/GameViewController.swift
@@ -30,7 +30,8 @@ class GameViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        if let scene = GameScene(fileNamed:"GameScene") {
+        //if let scene = GameScene(fileNamed:"GameScene") {
+        if let scene = MenuScene(fileNamed:"MenuScene") {
             // Configure the view.
             let skView = self.view as! SKView
             // For Debug
--- Your Email/MenuScene.swift
+++ Your Email/MenuScene.swift
@@ -1,9 +1,50 @@
 //
 //  MenuScene.swift
-//  Your Email
-//
-//  Created by Antonio Tsai on 06/01/2017.
-//  Copyright © 2017 Razeware LLC. All rights reserved.
-//
 
-import Foundation
+import SpriteKit
+// GameState is in GameplayKit
+// import GameplayKit
+
+class MenuScene: SKScene {
+    
+    //  Implement didBegin(_:) to handle the collisions
+    func didBegin(_ contact: SKPhysicsContact) {
+        
+    }
+    
+    override func didMove(to view: SKView) {
+        super.didMove(to: view)
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        // Enter label that touch the screen
+        let transition:SKTransition = SKTransition.fade(withDuration: 1)
+        var _:SKScene
+        
+        print("Touch Event!")
+        for touch in touches {
+            let location = (touch as UITouch).location(in: self)
+            if let theName = self.atPoint(location).name {
+                if theName == "newGame" {
+                    if let scene = GameScene(fileNamed:"GameScene") {
+                        self.view?.presentScene(scene, transition: transition)
+                    }
+                } else if theName == "scoreboard" {
+                    // Show Scoreboard
+                    /*
+                     if let scene = GameScene(fileNamed:"ScoreScene") {
+                        self.view?.presentScene(scene, transition: transition)
+                     }
+                     */
+                }
+            }
+        }
+    }
+    
+    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+    }
+
+    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+    }
+    
+}
