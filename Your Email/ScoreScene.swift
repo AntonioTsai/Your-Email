69959214c6d581822f1c304afc45bc7358cb6d81
Antonio Tsai <jimmyjack7777@gmail.com>
2017-01-13 05:23:15 +0800

    Adjust score scene layout

--- Your Email/ScoreScene.swift
+++ Your Email/ScoreScene.swift
@@ -20,7 +20,7 @@ class ScoreScene: SKScene {
         let lblScore = SKLabelNode(fontNamed: "ChalkboardSE-Bold")
         lblScore.fontSize = 60
         lblScore.fontColor = SKColor.white
-        lblScore.position = CGPoint(x: self.size.width / 2, y: 300)
+        lblScore.position = CGPoint(x: 0, y: 0)
         lblScore.horizontalAlignmentMode = SKLabelHorizontalAlignmentMode.center
         lblScore.text = String(format: "%d", GameState.sharedInstance.score)
         addChild(lblScore)
@@ -29,7 +29,7 @@ class ScoreScene: SKScene {
         let lblHighScore = SKLabelNode(fontNamed: "ChalkboardSE-Bold")
         lblHighScore.fontSize = 30
         lblHighScore.fontColor = SKColor.cyan
-        lblHighScore.position = CGPoint(x: self.size.width / 2, y: 150)
+        lblHighScore.position = CGPoint(x: 0, y: -150)
         lblHighScore.horizontalAlignmentMode = SKLabelHorizontalAlignmentMode.center
         lblHighScore.text = String(format: "High Score: %d", GameState.sharedInstance.highScore)
         addChild(lblHighScore)
@@ -38,7 +38,7 @@ class ScoreScene: SKScene {
         let lblTryAgain = SKLabelNode(fontNamed: "ChalkboardSE-Bold")
         lblTryAgain.fontSize = 30
         lblTryAgain.fontColor = SKColor.white
-        lblTryAgain.position = CGPoint(x: self.size.width / 2, y: 50)
+        lblTryAgain.position = CGPoint(x: 0, y: -200)
         lblTryAgain.horizontalAlignmentMode = SKLabelHorizontalAlignmentMode.center
         lblTryAgain.text = "Tap To Try Again"
         addChild(lblTryAgain)
